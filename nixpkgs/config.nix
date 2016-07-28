9646ebd7754d0ede1ebf58b1bfc1d7a31a020b66
Mike Cooper <mythmon@gmail.com>
2016-07-28 15:15:45 -0700

    Some nix stuff I'm about to delete

---
+++ nixpkgs/config.nix
@@ -0,0 +1,18 @@
+{
+  allowUnfree = true;
+  pulseaudio = true;
+
+  packageOverrides = pkgs: rec {
+    myEnv = pkgs.python.withPackages (ps: with ps; [ offlineimap pyyaml ]);
+
+		# Hold atom at 1.6.2, since it works.
+		atom = pkgs.lib.overrideDerivation pkgs.atom (attrs: {
+      name = "atom-1.6.2";
+      src = pkgs.fetchurl {
+        url = "https://github.com/atom/atom/releases/download/v1.6.2/atom-amd64.deb";
+        sha256 = "1kl2pc0smacn4lgk5wwlaiw03rm8b0763vaisgp843p35zzsbc9n";
+        name = "atom-1.6.2.deb";
+      };
+		});
+  };
+}
