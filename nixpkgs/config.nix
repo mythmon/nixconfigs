6086a17b0483e68797ffe5a0efe345f15b8d0815
Mike Cooper <mythmon@gmail.com>
2016-08-05 15:06:20 -0700

    Switch to allowUnfreePredicate

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,16 +1,20 @@
-{
-  allowUnfree = true;
+{ pkgs ? import <nixpkgs> }:
+
+rec {
+  allowUnfreePredicate = pkg: (
+    pkgs.lib.hasPrefix "firefox-" pkg.name
+  );
   pulseaudio = true;
 
   packageOverrides = pkgs: rec {
-		# Hold atom at 1.6.2, since it works.
-		atom = pkgs.lib.overrideDerivation pkgs.atom (attrs: {
+    # Hold atom at 1.6.2, since it works.
+    atom = pkgs.lib.overrideDerivation pkgs.atom (attrs: {
       name = "atom-1.6.2";
       src = pkgs.fetchurl {
         url = "https://github.com/atom/atom/releases/download/v1.6.2/atom-amd64.deb";
         sha256 = "1kl2pc0smacn4lgk5wwlaiw03rm8b0763vaisgp843p35zzsbc9n";
         name = "atom-1.6.2.deb";
       };
-		});
+    });
   };
 }
