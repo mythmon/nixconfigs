25b99809bbb6f1d17449546186af401b37643070
Mike Cooper <mythmon@gmail.com>
2016-08-21 01:16:09 +0000

    firefox-nightly-bin: Look for yesterday's build if today's can't be found

--- pkgs/firefox-nightly-bin/update.sh
+++ pkgs/firefox-nightly-bin/update.sh
@@ -11,6 +11,25 @@ BASE_FILENAME="firefox-$VERSION.$LOCALE.$ARCH"
 TARBZ="$BASE_FILENAME.tar.bz2"
 
 TS=$(curl -s $BASEURL/$YEAR/$MONTH/ | sed -ne "s/.*$YEAR-$MONTH-$DAY-\(..-..-..\)-mozilla-central\/.*/\\1/p" | head -n 1)
+
+if [[ -z $TS ]]; then
+  # Maybe today's build isn't ready, try yesterday's
+  ORIG_YEAR=$YEAR
+  ORIG_MONTH=$MONTH
+  ORIG_DAY=$DAY
+  YEAR=$(date --date=yesterday +%Y)
+  MONTH=$(date --date=yesterday +%m)
+  DAY=$(date --date=yesterday +%d)
+  echo "Can't find build for today, trying yesterday" >&2
+
+  TS=$(curl -s $BASEURL/$YEAR/$MONTH/ | sed -ne "s/.*$YEAR-$MONTH-$DAY-\(..-..-..\)-mozilla-central\/.*/\\1/p" | head -n 1)
+
+  if [[ -z $TS ]]; then
+   echo "Couldn't find mozilla-central build for $ORIG_YEAR-$ORIG_MONTH-$ORIG_DAY or $YEAR-$MONTH-$DAY at $BASEURL/"
+   exit 1
+ fi
+fi
+
 DIRURL="$BASEURL/$YEAR/$MONTH/$YEAR-$MONTH-$DAY-$TS-mozilla-central"
 SHA512=$(curl -s $DIRURL/$BASE_FILENAME.checksums | grep sha512 | grep "$TARBZ$" | cut -d ' ' -f 1-1)
 
