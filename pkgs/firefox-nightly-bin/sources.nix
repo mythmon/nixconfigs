8732a616ebca98f09ec081b854c3ef5ed1dd4d55
Mike Cooper <mythmon@gmail.com>
2016-09-12 16:39:35 +0000

    firefox-nightly-bin: update

--- pkgs/firefox-nightly-bin/sources.nix
+++ pkgs/firefox-nightly-bin/sources.nix
@@ -1,11 +1,11 @@
 {
-  version = "51.0a1-2016-09-08";
+  version = "51.0a1-2016-09-12";
   sources = [
     {
       locale = "en-US";
       arch = "linux-x86_64";
-      url = "https://archive.mozilla.org/pub/firefox/nightly/2016/09/2016-09-08-03-04-34-mozilla-central/firefox-51.0a1.en-US.linux-x86_64.tar.bz2";
-      sha512 = "7f34d05707724660babcc2fb81e5295fa1aef3240cf2810d257b0521eba5a03ee52d3a2700a7111e24c00646b2ff2776b77b84e727aac4a305897000f25223c9";
+      url = "https://archive.mozilla.org/pub/firefox/nightly/2016/09/2016-09-12-03-04-21-mozilla-central/firefox-51.0a1.en-US.linux-x86_64.tar.bz2";
+      sha512 = "e95879697b305e60402d566251f710de05c5ecb4859d415ca9e13e8fdeaebc8d5c32f53d3f87b1d02365ae7f6a458460715da5257e9523c19aa1c43ef12a7452";
     }
   ];
 }
