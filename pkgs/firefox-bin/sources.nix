e029c092c27d23acc78a398943936a0395c1f0a3
Mike Cooper <mythmon@gmail.com>
2016-11-08 15:28:05 -0800

    firefox-bin: update sources

--- pkgs/firefox-bin/sources.nix
+++ pkgs/firefox-bin/sources.nix
@@ -1,23 +1,23 @@
 {
   aurora = {
-    version = "51.0a2-2016-10-31";
+    version = "51.0a2-2016-11-8";
     sources = [
       {
         locale = "en-US";
         arch = "linux-x86_64";
-        url = "https://archive.mozilla.org/pub/firefox/nightly/2016/10/2016-10-31-00-40-02-mozilla-aurora/firefox-51.0a2.en-US.linux-x86_64.tar.bz2";
-        sha512 = "24de13ad9b918fcd0d8e5b0f8c78df34fc9b1143634b45bb7d836589f4bb1f06896193b9e072bf8331bda0dd2604273cd5e6f77aee806c697b4dc04163be5c45";
+        url = "https://archive.mozilla.org/pub/firefox/nightly/2016/11/2016-11-08-00-40-19-mozilla-aurora/firefox-51.0a2.en-US.linux-x86_64.tar.bz2";
+        sha512 = "ff827f3369898445b922a73cbccb51903fd1919d2d9ec9911e1a306cea6d37ff02b0cddc8488212999c54bb217b9c1536dca971582502b35e65aca8606c2e9bb";
       }
     ];
   };
   nightly = {
-    version = "52.0a1-2016-10-31";
+    version = "52.0a1-2016-11-8";
     sources = [
       {
         locale = "en-US";
         arch = "linux-x86_64";
-        url = "https://archive.mozilla.org/pub/firefox/nightly/2016/10/2016-10-31-03-02-02-mozilla-central/firefox-52.0a1.en-US.linux-x86_64.tar.bz2";
-        sha512 = "1b1faca57727e397dc12bc70d841d6c5c1e7e8016d6271c412fbf4e676c48f1f0b0af6e80edc44b3b532829207927d57829a346083c3df488ba8baf168f04799";
+        url = "https://archive.mozilla.org/pub/firefox/nightly/2016/11/2016-11-08-03-02-12-mozilla-central/firefox-52.0a1.en-US.linux-x86_64.tar.bz2";
+        sha512 = "0d5087612ea0ceedfe6646e03d465504f20de5c155a110eae9739f8689bf3542cdbd2aa335953fbb60f60cdea5cda57a6c0a7dfeb626fd57895749559263b766";
       }
     ];
   };
